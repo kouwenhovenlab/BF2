f5dd0ca816b437275eee85bd813432edefbc5eee
Majo Minions <majominions@github.com>
2018-10-23 15:23:12 +0200

    latest work (fluxonium)

---
+++ BF2/init_station.py
@@ -0,0 +1,8 @@
+# Setting up variables and such.
+
+import qcodes as qc
+from pytopo.mplplots.init_nb_plotting import *
+from pytopo.mplplots import plots
+from pytopo.mplplots import tools as plottools
+
+qc.config['core']['db_location'] = r"D:\OneDrive\BF2\Data\experiments.db"
--- BF2/init_station_cqed.py
+++ BF2/init_station_cqed.py
@@ -4,79 +4,17 @@ import qcodes as qc
 import broadbean as bb
 from pytopo.qctools import instruments as instools
 from pytopo.qctools.instruments import create_inst, add2station
+from pytopo.rf.alazar.acquisition_tools import simple_alazar_setup_ext_trigger
+
+from init_station import *
+
 
 # set some station configuration variables
-qc.config['core']['db_location'] = r"D:\OneDrive\BF2\Data\experiments.db"
 qc.config['user']['instruments'] = {
     'awg_name' : 'awg',
     'alazar_name' : 'alazar',
     'default_acquisition_controller' : 'post_iq_acq',
     }
-
-# simplified setting up and acquisition
-
-def setup_alazar9360_ext_trigger(nsamples, nrecords, nbuffers, 
-                                 allocated_buffers=2, 
-                                 SR=2e8, int_time=None):
-    """
-    Simple setting up of the alazar. This is basically just setting some
-    reasonable starting values when starting up the station.
-
-    Parameters:
-    -----------
-    nsamples : int
-        samples per record
-
-    nrecords : int
-        records per buffer
-
-    nbuffers : int
-        buffers per acquisition
-
-    allocated_buffers : int (default: 2)
-        allocated buffers
-
-    SR : float (default: 2e8)
-        sampling rate
-
-    int_time : float (default: None)
-        if not None, will try to compute number of samples that best corresponds
-        to this measurement time (taking mod 128 into account, and that we need at
-        least 256 samples per record). Overrrides nsamples if set.
-    """
-    
-    alazar = qc.Instrument.find_instrument('alazar')
-    
-    SR = int(SR)
-    if int_time is not None:
-        SPR = max(256, int(int_time * SR // 128 * 128))
-    else: 
-        SPR = nsamples
-    
-    with alazar.syncing():
-        alazar.clock_source('INTERNAL_CLOCK')
-        alazar.sample_rate(SR)
-        alazar.clock_edge('CLOCK_EDGE_RISING')
-        alazar.decimation(1)
-        alazar.coupling1('DC')
-        alazar.coupling2('DC')
-        alazar.channel_range1(0.4)
-        alazar.channel_range2(0.4)
-        alazar.impedance1(50)
-        alazar.impedance2(50)
-        alazar.trigger_source1('EXTERNAL')
-        alazar.trigger_level1(128 + 5)
-        alazar.external_trigger_coupling('DC')
-        alazar.external_trigger_range('ETR_TTL')
-        alazar.trigger_delay(0)
-        # alazar.timeout_ticks(int(1e7))
-        alazar.timeout_ticks(int(0))
-        alazar.records_per_buffer(nrecords)
-        alazar.buffers_per_acquisition(nbuffers)
-        alazar.buffer_timeout(10000)
-        alazar.samples_per_record(SPR)
-        alazar.allocated_buffers(allocated_buffers)
-
 
 # init instruments lives in a function (to be able to import without loading instruments.)
 def init_instruments():
@@ -90,7 +28,7 @@ def init_instruments():
     alazar = instools.create_inst(AlazarTech_ATS9360, 'alazar', force_new_instance=True)
     inst_list.append(alazar)
 
-    setup_alazar9360_ext_trigger(256, 1, 1)
+    simple_alazar_setup_ext_trigger(256, 1, 1)
     
     # create and setup AWG
     from qcodes.instrument_drivers.tektronix.AWG5208 import AWG5208
